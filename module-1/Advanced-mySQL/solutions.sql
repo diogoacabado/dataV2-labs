fdc7d28e08a549eb57bdde7d553278ccc179ea96
diogo_acabado <diogoacabadofm@gmail.com>
2020-02-29 17:31:24 +0100

    adv_sql

--- module-1/Advanced-mySQL/solutions.sql
+++ module-1/Advanced-mySQL/solutions.sql
@@ -1,3 +1,5 @@
+# Challenge 1
+
 # Step 1
 SELECT 
 titleauthor.au_id, 
@@ -11,7 +13,11 @@ INNER JOIN sales
 ON titles.title_id = sales.title_id;
 
 # Step 2
-SELECT tbl.au_id, tbl.title_id, SUM(tbl.sales_royalty), tbl.advance
+SELECT 
+tbl.au_id, 
+tbl.title_id, 
+tbl.advance, 
+SUM(tbl.sales_royalty) AS sales_total
 FROM (
 	SELECT 
 	titleauthor.au_id, 
@@ -23,15 +29,15 @@ FROM (
 	ON titleauthor.title_id = titles.title_id
 	INNER JOIN sales
 	ON titles.title_id = sales.title_id) tbl
-GROUP BY tbl.au_id, tbl.title_id;
+GROUP BY tbl.au_id, tbl.title_id, advance;
 
 # Step 3
-SELECT sub_table.au_id, SUM(sub_table.sum_sales_royalty) + SUM(sub_table.advance) AS total
+SELECT sub_table.au_id, SUM(sub_table.sales_total) + SUM(sub_table.advance) AS total
 FROM(
 	SELECT 
 	tbl.au_id, 
 	tbl.title_id, 
-	SUM(tbl.sales_royalty) AS sum_sales_royalty, 
+	SUM(tbl.sales_royalty) AS sales_total, 
 	tbl.advance AS advance
 	FROM(
 		SELECT
@@ -44,8 +50,65 @@ FROM(
 		ON titleauthor.title_id = titles.title_id
 		INNER JOIN sales
 		ON titles.title_id = sales.title_id) tbl
-	GROUP BY tbl.au_id, tbl.title_id) sub_table
+	GROUP BY tbl.au_id, tbl.title_id, advance) sub_table
 GROUP BY sub_table.au_id
 ORDER BY total DESC
 LIMIT 3;
+
+
+# Challenge 2
+
+# Step 1
+
+CREATE TEMPORARY TABLE temp_1
+SELECT
+titleauthor.au_id, 
+titleauthor.title_id,
+titles.advance * titleauthor.royaltyper / 100 AS advance, 
+titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 AS sales_royalty
+FROM titleauthor
+INNER JOIN titles
+ON titleauthor.title_id = titles.title_id
+INNER JOIN sales
+ON titles.title_id = sales.title_id;
+
+SELECT *
+FROM temp_1;
+
+# Step 2
+
+CREATE TEMPORARY TABLE temp_2
+SELECT 
+au_id, 
+title_id,
+advance,
+SUM(sales_royalty) AS sales_total
+FROM temp_1
+GROUP BY au_id, title_id, advance;
+
+SELECT *
+FROM temp_2;
+
+# Step 3
+SELECT 
+au_id, 
+SUM(sales_total) + SUM(advance) AS total
+FROM temp_2
+GROUP BY au_id
+ORDER BY total DESC
+LIMIT 3;
+
+# Challenge 3
+
+CREATE TABLE most_profiting_authors
+SELECT 
+au_id, 
+SUM(sales_total) + SUM(advance) AS profits
+FROM temp_2
+GROUP BY au_id
+ORDER BY profits DESC
+LIMIT 3;
+
+SELECT *
+FROM most_profiting_authors;
 
